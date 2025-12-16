091c4208e6c4da244d09ce0e696808c8daeedc36
harishsehlangia <harish82817@gmail.com>
2025-12-16 22:02:18 +0530

    feat(editor): add editor page navbar

--- frontend/src/components/user-navigation.component.jsx
+++ frontend/src/components/user-navigation.component.jsx
@@ -20,7 +20,7 @@ const UserNavigationPanel = () => {
         >
             <div className="bg-white absolute right-0 border border-grey w-60 duration-200">
                 
-                <Link to="/" className="flex gap-2 link md:hidden pl-8 py-4">
+                <Link to="/editor" className="flex gap-2 link md:hidden pl-8 py-4">
                     <i className="fi fi-rr-file-edit"></i>
                     <p>Write`</p>
                 </Link>
