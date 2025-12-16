44e1c240c71472114e2c886b37ae02c2c6300ed4
harishsehlangia <harish82817@gmail.com>
2025-12-16 18:58:46 +0530

    feat(editor): setup /editor route and editor page component

---
+++ frontend/src/components/blog-editor.component.jsx
@@ -0,0 +1,7 @@
+const BlogEditor = () => {
+    return(
+        <h1>Blog Editor - From Component</h1>
+    )
+}
+
+export default BlogEditor;
---
+++ frontend/src/components/publish-form.component.jsx
@@ -0,0 +1,7 @@
+const PublishForm = () => {
+    return(
+        <h1>Publish Form - from Component</h1>
+    )
+}
+
+export default PublishForm;
