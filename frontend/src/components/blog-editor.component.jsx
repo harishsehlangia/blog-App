565285374eb831499d30831c9e2a891aa22503a9
harishsehlangia <harish82817@gmail.com>
2025-12-18 21:58:11 +0530

    feat(editor): add blog title input and text texture handling

--- frontend/src/components/blog-editor.component.jsx
+++ frontend/src/components/blog-editor.component.jsx
@@ -35,6 +35,19 @@ const BlogEditor = () => {
         
     }
 
+    const handleTitleKeyDown = (e) => {
+        if(e.keyCode == 13){
+            e.preventDefault();
+        }
+    }
+
+    const handleTitleChange = (e) => {
+        let input = e.target;
+
+        input.style.height = 'auto';
+        input.style.height = input.scrollHeight + "px";
+    }
+
     return(
         <>
             <nav className="navbar">
@@ -75,6 +88,14 @@ const BlogEditor = () => {
                             </label>
                             
                         </div>
+
+                        <textarea
+                            placeholder="Blog Title"
+                            className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40"
+                            onKeyDown={handleTitleKeyDown}
+                            onChange={handleTitleChange}
+                        ></textarea>
+
                     </div>
                 </section>
             </AnimationWrapper>
