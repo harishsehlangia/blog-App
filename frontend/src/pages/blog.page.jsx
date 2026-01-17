03e38f919bac4963533341a4be8fd7b8ba673cd0
harishsehlangia <harish82817@gmail.com>
2026-01-17 23:10:17 +0530

    feat(server): implement get blog endpoint

--- frontend/src/pages/blog.page.jsx
+++ frontend/src/pages/blog.page.jsx
@@ -1,6 +1,11 @@
+import { useParams } from "react-router-dom";
+
 const BlogPage = () => {
+
+    let { blog_id } = useParams()
+
     return (
-        <h1> Blogs will be shown here </h1>
+        <h1> {blog_id} </h1>
     )
 }
 
