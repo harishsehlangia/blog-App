9d05d011884fc0dda6a75f014bce0f128c3d7e08
harishsehlangia <harish82817@gmail.com>
2026-01-20 18:29:18 +0530

    feat(blog): integrate get blog API on frontend

--- frontend/src/pages/blog.page.jsx
+++ frontend/src/pages/blog.page.jsx
@@ -1,11 +1,43 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
+export const blogStructure = {
+    title: '',
+    des: '',
+    content: [],
+    tags: [],
+    author: { personal_info: { } },
+    banner: '',
+    publishedAt: ''
+}
 
 const BlogPage = () => {
 
     let { blog_id } = useParams()
 
+    const [ blog, setBlog ] = useState(blogStructure);
+
+    let { title, content, banner, author: { personal_info: { fullname, username, profile_img } }, publishedAt } = blog;
+
+    const fetchBlog = () => {
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-blog", { blog_id })
+        .then(({ data: { blog } }) => {
+            setBlog(blog);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
+    useEffect(() => {
+
+        fetchBlog();
+
+    }, [])
+
     return (
-        <h1> {blog_id} </h1>
+        <h1> This is hte blog - {blog.title} </h1>
     )
 }
 
