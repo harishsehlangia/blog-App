8bc77ddf4868e1cf907dc76ddf9715623dffab26
harishsehlangia <harish82817@gmail.com>
2026-01-23 19:31:33 +0530

    fix(blog): prevent HTML entities like &nbsp; from leaking into UI

--- frontend/src/components/blog-content.component.jsx
+++ frontend/src/components/blog-content.component.jsx
@@ -3,7 +3,7 @@ const Img = ({ url, caption }) => {
         <div>
             <img src={url} />
             {
-                caption.length ? <p className="w-full text-center my-3 md:mb-12 text-base text-dark-grey">{caption}</p> : ""
+                caption.length ? <p className="w-full text-center my-3 md:mb-12 text-base text-dark-grey">{cleanHTML(caption)}</p> : ""
             }
         </div>
     )
@@ -12,9 +12,9 @@ const Img = ({ url, caption }) => {
 const Quote = ({ quote, caption }) => {
     return (
         <div className="bg-purple/10 p-3 pl-5 border-l-4 border-purple">
-            <p className="text-xl leading-10 md:text-2xl">{quote}</p>
+            <p className="text-xl leading-10 md:text-2xl">{cleanHTML(quote)}</p>
             {
-                caption.length ? <p className="w-full text-purple text-base">{caption}</p> : ""
+                caption.length ? <p className="w-full text-purple text-base">{cleanHTML(caption)}</p> : ""
             }
         </div>
     )
@@ -26,7 +26,7 @@ const List = ({ style, items }) => {
 
             {
                 items.map((listItems, i) => {
-                    return <li key={i} className="my-4" dangerouslySetInnerHTML={{ __html: listItems }}></li>
+                    return <li key={i} className="my-4" dangerouslySetInnerHTML={{ __html: cleanHTML(listItems) }}></li>
                 })
             }
 
@@ -34,19 +34,24 @@ const List = ({ style, items }) => {
     )
 }
 
+// function to handle spaces in quotes.
+const cleanHTML = (html = "") => {
+    return html.replace(/&nbsp;/g, " ").trim();
+};
+
 const BlogContent = ({ block }) => {
 
     let { type, data } = block;
 
     if(type == "paragraph"){
-        return <p dangerouslySetInnerHTML={{ __html: data.text }}></p>
+        return <p dangerouslySetInnerHTML={{ __html: cleanHTML(data.text) }}></p>
     }
 
     if(type == "header"){
         if(data.level == 3){
-            return <h3 className="text-3xl font-bold" dangerouslySetInnerHTML={{ __html: data.text }}></h3>
+            return <h3 className="text-3xl font-bold" dangerouslySetInnerHTML={{ __html: cleanHTML(data.text) }}></h3>
         }
-        return <h2 className="text-4xl font-bold" dangerouslySetInnerHTML={{ __html: data.text }}></h2>
+        return <h2 className="text-4xl font-bold" dangerouslySetInnerHTML={{ __html: cleanHTML(data.text) }}></h2>
     }
 
     if(type == "image"){
