6ccb0726a7ebc223554ef014315453ccfcf571fc
harishsehlangia <harish82817@gmail.com>
2026-01-31 22:34:29 +0530

    feat(comment): add load more replies functionality

--- frontend/src/components/comment-card.component.jsx
+++ frontend/src/components/comment-card.component.jsx
@@ -25,6 +25,7 @@ const CommentCard = ({ index, leftVal, commentData }) => {
         } catch{
             startingPoint = undefined;
         }
+        return startingPoint;
     }
 
     const removeCommentsCards = (startingPoint, isDelete = false) => {
@@ -65,22 +66,22 @@ const CommentCard = ({ index, leftVal, commentData }) => {
 
     }
 
-    const loadReplies = ({ skip = 0 }) => {
+    const loadReplies = ({ skip = 0, currentIndex = index }) => {
 
-        if(children.length){
+        if(commentsArr[currentIndex].children.length){
             
             hideReplies();
 
-            axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-replies", { _id, skip })
+            axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-replies", { _id: commentsArr[currentIndex]._id, skip })
             .then(({ data: { replies } }) => {
 
-                commentData.isReplyLoaded = true;
+                commentsArr[currentIndex].isReplyLoaded = true;
 
                 for(let i=0; i<replies.length; i++){
 
-                    replies[i].childrenLevel = commentData.childrenLevel + 1;
+                    replies[i].childrenLevel = commentsArr[currentIndex].childrenLevel + 1;
 
-                    commentsArr.splice(index + 1 + i + skip, 0, replies[i])
+                    commentsArr.splice(currentIndex + 1 + i + skip, 0, replies[i])
 
                 }
 
@@ -129,6 +130,35 @@ const CommentCard = ({ index, leftVal, commentData }) => {
         }
 
         setReplying(preVal => !preVal);
+
+    }
+
+    const LoadMoreRepliesBtn = () => {
+
+        let parentIndex = getParentIndex();
+
+        let button = <button 
+                        onClick={() => loadReplies({ skip: index - parentIndex, currentIndex: parentIndex })}
+                        className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2"
+                    >Load More Replies</button>
+
+        if(commentsArr[index + 1]){
+            if(commentsArr[index + 1].childrenLevel < commentsArr[index].childrenLevel){
+                if((index - parentIndex) < commentsArr[parentIndex].children.length){
+
+                    return button;
+                
+                }
+            }
+        } else{
+            if(parentIndex){
+                if((index - parentIndex) < commentsArr[parentIndex].children.length){
+
+                    return button;
+                
+                }
+            }
+        }
 
     }
 
@@ -187,6 +217,8 @@ const CommentCard = ({ index, leftVal, commentData }) => {
                 }
 
             </div>
+
+            <LoadMoreRepliesBtn />
         </div>
     )
 }
