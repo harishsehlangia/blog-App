8231444d9ed967324108e405d17bc17ab20438e6
harishsehlangia <harish82817@gmail.com>
2026-04-20 18:53:08 +0530

    feat(blog): add API to fetch user written blogs with pagination and search

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import SideNav from "./components/sidenavbar.component";
 import ChangePassword from "./pages/change-password.page";
 import EditProfile from "./pages/edit-profile.page";
 import Notifications from "./pages/notifications.page";
+import ManageBlogs from "./pages/manage-blogs.page";
 
 export const UserContext = createContext({})
 
@@ -36,6 +37,7 @@ const App = () => {
                 <Route path="/" element={<Navbar />} >
                     <Route index element={<HomePage />} />
                     <Route path="dashboard" element={<SideNav />} >
+                        <Route path="blogs" element={<ManageBlogs />} />
                         <Route path="notifications" element={<Notifications />} />
                     </Route>
                     <Route path="settings" element={<SideNav />} >
---
+++ frontend/src/pages/manage-blogs.page.jsx
@@ -0,0 +1,7 @@
+const ManageBlogs = () => {
+    return (
+        <h1>Again BlogPageeeeeeee</h1>
+    )
+}
+
+export default ManageBlogs;
