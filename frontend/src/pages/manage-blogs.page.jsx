2a55eecd0efb6e8b317657af1bdfbdf79dba0f53
harishsehlangia <harish82817@gmail.com>
2026-04-21 18:39:05 +0530

    feat(blog): implement blog management UI with published blogs list

--- frontend/src/pages/manage-blogs.page.jsx
+++ frontend/src/pages/manage-blogs.page.jsx
@@ -2,6 +2,12 @@ import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../App";
 import { filterPaginationData } from "../common/filter-pagination-data";
 import axios from "axios";
+import { Toaster } from "react-hot-toast";
+import InPageNavigation from "../components/inpage-navigation.component";
+import Loader from "../components/loader.component";
+import NoDataMessage from "../components/nodata.component";
+import AnimationWrapper from "../common/page-animation";
+import ManagePublishedBlogsCard from "../components/manage-blogcard.component";
 
 const ManageBlogs = () => {
 
@@ -56,8 +62,70 @@ const ManageBlogs = () => {
 
     }, [access_token, blogs, drafts, query])
 
+    const handleChange = (e) => {
+        if(!e.target.value.length){
+            setQuery("");
+            setBlogs(null);
+            setDrafts(null);
+        }
+    }
+
+    const handleSearch = (e) => {
+        let searchQuery = e.target.value;
+
+        setQuery(searchQuery);
+
+        if(e.keyCode == 13 && searchQuery.length){
+            setBlogs(null);
+            setDrafts(null);
+        }
+    }
+
     return (
-        <h1>Again BlogPageeeeeeee</h1>
+        <>
+            <h1 className="max-md:hidden">Manage Blogs</h1>
+
+            <Toaster />
+
+            <div className="relative max-md:mt-5 md:mt-8 mb-10">
+                <input 
+                    type="search"
+                    className="w-full bg-grey p-4 pl-12 pr-6 rounded-full placeholder:text-dark-grey"
+                    placeholder="Search Blogs"
+                    onChange={handleChange}
+                    onKeyDown={handleSearch}
+                />
+
+                <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey "></i>
+            </div>
+
+            <InPageNavigation routes={["Published Blogs", "Draft Blogs"]}>
+
+                {
+
+                    blogs == null ? <Loader /> : 
+                    blogs.results.length ? 
+                        
+                        <>
+                        {
+                            blogs.results.map((blog, i) => {
+                                return <AnimationWrapper key={i} transition={{ delay: i * 0.04 }}>
+                                    
+                                    <ManagePublishedBlogsCard blog={blog} />
+
+                                </AnimationWrapper>
+                            })
+                        }
+                        </>
+                    
+                    : <NoDataMessage message="No published blogs" />
+
+                }
+                <h1>This is Drafttttttttttt</h1>
+
+            </InPageNavigation>
+
+        </>
     )
 }
 
