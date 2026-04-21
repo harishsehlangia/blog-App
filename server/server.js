2e35bed92559904ac88f0820a09c8a997b6ae717
harishsehlangia <harish82817@gmail.com>
2026-04-21 21:42:26 +0530

    feat(blog): add API to delete the blog

--- server/server.js
+++ server/server.js
@@ -973,6 +973,31 @@ server.post("/user-written-blogs-count", verifyJWT, (req, res) => {
 
 })
 
+// delete blog
+server.post("/delete-blog", verifyJWT, (req, res) => {
+
+    let user_id = req.user;
+    let blog_id = req.body;
+
+    Blog.findOneAndDelete({ blog_id })
+    .then(blog => {
+
+        Notification.deleteMany({ blog: blog._id }).then(data => console.log("notification deleted"));
+
+        Comment.deleteMany({ blog_id: blog._id }).then(data => console.log("comments deleted"));
+
+        User.findOneAndUpdate({ _id: user_id }, { $pull: { blog: blog._id }, $inc: { "account_info.total_posts": -1 } })
+        .then(user => console.log("Blog deleted"));
+
+        return res.status(200).json({ status: "done" });
+
+    })
+    .catch(err => {
+        return res.status(500).json({ error: err.message });
+    })
+
+})
+
 server.listen(PORT, ()=>{
     console.log('listening on port : ' + PORT);    
 })
